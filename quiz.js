38ae2b92436e77d7f46f9407dcfd0b99b2267b58
Karuna-s-anil <119606620+Karuna-s-anil@users.noreply.github.com>
2024-08-27 23:02:28 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,64 @@
+var data=[
+
+{
+	"qs":"Q1. Which planet is known as the Red planet?",
+	"options":[
+		"a) Earth",
+		"b) Mars",
+		"c) Venus",
+		"d) Jupiter"
+
+
+		],
+	"answerId":1
+},
+{
+	"qs":"Q2. What is the smallest prime number?",
+	"options":[
+		"a) 4",
+		"b) 1",
+		"c) 2",
+		"d) 5"
+
+
+		],
+	"answerId":2	
+},
+{
+	"qs":"Q3. What is the capital city of France?",
+	"options":[
+		"a) Rome",
+		"b) Berlin",
+		"c) Madrid",
+		"d) Paris"
+
+
+		],
+	"answerId":3
+},
+{
+	"qs":"Q4. Which is the only body part that is fully grown from birth?",
+	"options":[
+		"a) Eyes",
+		"b) Ears",
+		"c) Nose",
+		"d) Hands"
+
+
+		],
+	"answerId":0
+},
+{
+	"qs":"Q5. Which planet in our Solar System is known for having a ring?",
+	"options":[
+		"a) Mercury",
+		"b) Saturn",
+		"c) Venus",
+		"d) Mars"
+
+
+		],
+	"answerId":1
+}
+
+]
